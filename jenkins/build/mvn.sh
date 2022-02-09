2fd484a59f833ad7de427bb9ec98f79bc07d9ff4
EC2 Default User <ec2-user@ip-172-31-12-255.ap-south-1.compute.internal>
2022-02-09 07:54:04 +0000

    fix path

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
 echo $PWD
-docker run --rm -v $PWD/my-app:/app -v /root/.m2/:/root/.m2//root/.m2/ -w /app maven:3-alpine "$@"
+WORKSPACE=/var/lib/jenkins/workspace/pipeline
+
+docker run --rm -v $WORKSPACE/my-app:/app -v /root/.m2/:/root/.m2//root/.m2/ -w /app maven:3-alpine "$@"
